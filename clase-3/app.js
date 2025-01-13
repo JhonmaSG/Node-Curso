23337e650dc049b745bb042b87b60c5393d661b2
JhonmaSG <jmserrnaog@udistrital.edu.co>
2025-01-13 15:16:26 -0500

    Completed GET and POST, fix PATCH

--- clase-3/app.js
+++ clase-3/app.js
@@ -1,11 +1,21 @@
 const express = require('express') // require -> commonJS
+const crypto = require('node:crypto')
 const movies = require('./movies.json')
+const { validateMovie } = require('./schemas/movies')
 
 const app = express()
+app.use(express.json())
 app.disable('x-powered-by')
 
 // Todos los recursos que sean MVOIES se identifican con /movies
 app.get('/movies', (req, res) => {
+  const { genre } = req.query
+  if (genre) {
+    const filteredMovies = movies.filter(
+      movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase())
+    )
+    return res.json(filteredMovies)
+  }
   res.json(movies)
 })
 
@@ -13,9 +23,38 @@ app.get('/movies', (req, res) => {
 app.get('/movies/:id', (req, res) => { // path-to-regexp
   const { id } = req.params
   const movie = movies.find(movie => movie.id === id)
-  if (movie) return res.json(movie)
 
+  if (movie) return res.json(movie)
+  // 422 Unprocessable Entity
   res.status(404).json({ message: 'Movie not found' })
+})
+
+app.post('/movies', (req, res) => {
+  const result = validateMovie(req.body)
+
+  if (result.error) {
+    return res.status(400).json({ error: result.error.message })
+  }
+
+  // en base de datos
+  const newMovie = {
+    id: crypto.randomUUID(), // uuid v4
+    ...result.data
+    /* // quitar si se hacen las validaciones correctamente, siendo asi el uso de ...result.body
+    title,
+    genre,
+    director,
+    year,
+    duration,
+    rate: rate ?? 0,
+    poster */
+  }
+
+  // Esto no seria REST, ya que se estan guardando
+  // el estado de la app en memoria
+  movies.push(newMovie)
+
+  res.status(201).json(newMovie) // actualizar la caché del cliente
 })
 
 const PORT = process.env.PORT ?? 1234
