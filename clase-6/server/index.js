5d3cc922dfdf3cef9af4e7135d74309015c318fb
JhonmaSG <jmserrnaog@udistrital.edu.co>
2025-02-25 13:59:15 -0500

    completed chat socket, fix deploy future

--- clase-6/server/index.js
+++ clase-6/server/index.js
@@ -1,8 +1,12 @@
 import express from 'express'
 import logger from 'morgan'
+import dotenv from 'dotenv'
+import { createClient } from '@libsql/client'
 
 import { Server } from 'socket.io'
 import { createServer } from 'node:http'
+
+dotenv.config()
 
 const port = process.env.PORT ?? 3000
 
@@ -12,22 +16,64 @@ const io = new Server(server, {
   connectionStateRecovery: {}
 })
 
-io.on('connection', (socket) => {
-  console.log('a user has connected!.')
+// Conexión a la base de datos
+const db = createClient({
+  url: 'libsql://busy-vixen-jhonmasg.turso.io',
+  authToken: process.env.DB_TOKEN
+})
+
+// CREACION DE LA BD
+await db.execute(`
+  CREATE TABLE IF NOT EXISTS messages (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    content TEXT,
+    user TEXT
+  )
+`)
+
+io.on('connection', async (socket) => {
+  console.log('a user has connected!')
 
   socket.on('disconnect', () => {
     console.log('an user has disconnected')
   })
 
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg)
+  socket.on('chat message', async (msg) => {
+    let result
+    const username = socket.handshake.auth.username ?? 'anonymous'
+    console.log({ username })
+    try {
+      result = await db.execute({
+        sql: 'INSERT INTO messages (content, user) VALUES (:msg, :username)',
+        args: { msg, username }
+      })
+    } catch (e) {
+      console.error(e)
+      return
+    }
+
+    io.emit('chat message', msg, result.lastInsertRowid.toString(), username)
   })
+
+  if (!socket.recovered) { // <- recuperase los mensajes sin conexión
+    try {
+      const results = await db.execute({
+        sql: 'SELECT id, content, user FROM messages WHERE id > ?',
+        args: [socket.handshake.auth.serverOffset ?? 0]
+      })
+
+      results.rows.forEach(row => {
+        socket.emit('chat message', row.content, row.id.toString(), row.user)
+      })
+    } catch (e) {
+      console.error(e)
+    }
+  }
 })
 
 app.use(logger('dev'))
 
 app.get('/', (req, res) => {
-  // Currend Worked directory
   res.sendFile(process.cwd() + '/client/index.html')
 })
 
